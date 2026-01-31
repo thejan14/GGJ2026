e89b7a0881371238fecd712e065bc42758e3ce9f
Jan Oberpichler <jan.oberpichler@googlemail.com>
2026-01-31 21:43:55 +0100

    fix info grid

--- scenes/result_info.gd
+++ scenes/result_info.gd
@@ -6,10 +6,10 @@ extends Control
 @export var info_grid: GridContainer
 
 func set_result(result: Array[PackedInt32Array]) -> void:
-	for child in get_children():
+	for child in info_grid.get_children():
 		child.queue_free()
 	for row in result:
 		for info in row:
 			var block = TextureRect.new()
 			block.texture = result_sprites[info]
-			add_child(block)
+			info_grid.add_child(block)
