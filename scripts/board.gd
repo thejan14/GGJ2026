16c21b96fc651043638b336cf505c437c1d4735c
Jan Oberpichler <jan.oberpichler@googlemail.com>
2026-01-31 20:46:06 +0100

    remove sprite grid from board

--- scripts/board.gd
+++ scripts/board.gd
@@ -2,7 +2,6 @@ class_name Board
 
 extends Grid
 
-var cells: Array[Array] = []
 var highlightCells: Array[Vector2i]
 var enemy_action: Vector2i = Vector2i(-1, -1)
 
@@ -13,30 +12,34 @@ func _input(event: InputEvent) -> void:
 			pass
 
 func _process(delta: float) -> void:
-	for row in cells:
-		for cell in row:
-			var sprite: Sprite2D = cell
-			sprite.modulate = Color(Color.WHITE, 0.4)
 	queue_redraw()
-	#for cell in highlightCells:
-		#highlight(cell)
-
-
-func highlight(target_cell :Vector2i):
-	if isValidPos(target_cell):
-		cells[target_cell.x][target_cell.y].modulate = Color.RED
 
 func _on_action_hint(new_action_hint: Rect2i) -> void:
 	action_hint = new_action_hint
 
 func _ready() -> void:
 	MultiplayerManager.action_hint.connect(_on_action_hint.bind())
-	for i in range(0, DIM):
-		var row: Array = []
-		for j in range(0, DIM):
-			var cell := Sprite2D.new()
-			cell.position = Vector2(i * CELL_SIZE, j * CELL_SIZE) + (Vector2.ONE * (CELL_SIZE / 2.0))
-			cell.texture = preload("uid://d0dksl0vaxqvn")
-			add_child(cell)
-			row.push_back(cell)
-		cells.push_back(row)
+	#for i in range(0, DIM):
+	#	var row: Array = []
+	#	for j in range(0, DIM):
+	#		var cell := Sprite2D.new()
+	#		cell.position = Vector2(i * CELL_SIZE, j * CELL_SIZE) + (Vector2.ONE * (CELL_SIZE / 2.0))
+	#		cell.texture = preload("uid://d0dksl0vaxqvn")
+	#		add_child(cell)
+	#		row.push_back(cell)
+	#	cells.push_back(row)
+
+func _draw() -> void:
+	super()
+	var start := 0
+	var end := CELL_SIZE * DIM
+	var color := Color(Color.BLACK, 0.4)
+	for i in range(0, DIM + 1):
+		var row := i * CELL_SIZE
+		draw_line(Vector2(row, start), Vector2(row, end), color, 2.0)
+	for j in range(0, DIM + 1):
+		var column := j * CELL_SIZE
+		draw_line(Vector2(start, column), Vector2(end, column), color, 2.0)
+	#for pos in highlightCells:
+	#	if isValidPos(pos):
+	#		draw_rect(Rect2(pos * CELL_SIZE, Vector2(CELL_SIZE, CELL_SIZE)), Color.RED)
